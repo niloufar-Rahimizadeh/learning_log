e1347eff7798f0211f2edcc2c61e2ce43e5f3788
rasulalizadeh <mohammadr.alizadeh@live.com>
2021-09-01 16:19:26 +0430

    10Shah

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Topic, Entry
-admin.site.register(Topic)
-admin.site.register(Entry)
+from .models import Topic
 # Register your models here.
+
+admin.site.register(Topic)
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,32 +1,3 @@
-from django.shortcuts import render,redirect
-from .models import Topic
-from .forms import TopicForm
+from django.shortcuts import render
+
 # Create your views here.
-
-def index(request):
-    return render(request, 'learning_logs/index.html')
-
-
-def topics(request):
-    topics = Topic.objects.order_by("added_date")
-    context = {'topics': topics}
-    return render(request, 'learning_logs/topics.html', context)
-
-
-def topic(request, topic_id):
-    topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by('-date_added')
-    context = {'topic': topic, 'entries': entries}
-    return render(request, 'learning_logs/topic.html', context)
-
-
-def new_topic(request):
-    if request.method != 'POST':
-        form = TopicForm()
-    else:
-        form = TopicForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('learning_logs: topics')
-    context = {'form': form}
-    return render(request, 'learning_logs/new_topic.html', context)
