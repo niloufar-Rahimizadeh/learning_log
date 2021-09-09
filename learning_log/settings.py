e2cb4b1428ecc36f3c9c37338ff4269453023545
Niloufar_rahimizadeh <niloufarrahimizade@yahoo.com>
2021-09-09 17:44:12 +0430

    18Shah

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'learning_logs',
     'users',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'users:login'
+LOGOUT_REDIRECT_URL = '/'
